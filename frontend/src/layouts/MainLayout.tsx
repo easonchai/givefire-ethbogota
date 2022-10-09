cc3eea66ca497462bc44ab9adda6a9c085069bad
Eason Chai <echai2905@gmail.com>
2022-10-09 08:08:37 -0500

    Fix layout

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -5,17 +5,20 @@ import { clsx } from "clsx";
 interface IMainLayoutProps {
   children: any;
   className?: string;
+  bg?: string;
 }
 
 export const MainLayout = ({
   children,
   className,
+  bg = "bg-primary",
 }: IMainLayoutProps): JSX.Element => {
   return (
     <div
       className={clsx(
-        "min-h-screen min-w-screen w-full h-full bg-primary flex flex-col",
-        className
+        "min-h-screen min-w-screen w-full h-full flex flex-col",
+        className,
+        bg
       )}
     >
       {children}
